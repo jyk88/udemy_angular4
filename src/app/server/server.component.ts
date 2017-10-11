5b24eb4d437744955531b9efd294b40800c332da
Jiyoon Koo <JKoo@ftc-llc.com>
2017-10-11 17:46:50 -0400

    end of day, section 2 lecture 27/28

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -12,5 +12,9 @@ in the index.html file
 
 // decorator : enhance your component "class"
 export class ServerComponent {
-
+    serverId: number = 10;
+    serverStatus: string = 'offline';
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,14 +4,28 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `<app-server></app-server><app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'no server was created! :(';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer()  {
+    this.serverCreationStatus = 'server was created!'
+  }
+
+  onUpdateServerName() {
+    
+  }
 }
