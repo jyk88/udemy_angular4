a78067001b73f8b6eef46aeb9564e30969056e73
Jiyoon Koo <JKoo@ftc-llc.com>
2017-10-16 16:26:18 -0400

    end of section 3, working on the project

--- project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../../recipes/recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'fried chicken',
+      'yummy dead animals!',
+      'https://cdn2.tmbi.com/TOH/Images/Photos/37/1200x1200/Crispy-Fried-Chicken_exps6445_PSG143429D03_05_5b_RMS.jpg'),
+    new Recipe(
+      'fried chicken',
+      'yummy dead animals!',
+      'https://cdn2.tmbi.com/TOH/Images/Photos/37/1200x1200/Crispy-Fried-Chicken_exps6445_PSG143429D03_05_5b_RMS.jpg')
+  ];
 
   constructor() { }
 
---
+++ project/src/app/shared/ingredient.model.ts
@@ -0,0 +1,10 @@
+export class Ingredient {
+
+  // shorter way of declaring name and amount
+
+  constructor(public name: string, public amount: number) {
+    this.name = name;
+    this.amount = amount;
+  }
+
+}
